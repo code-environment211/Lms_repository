a4ebd3007c6805e028ec04349f31cdece88a92fd
code-environment211 <mylenovo302025@gmail.com>
2025-06-18 17:54:41 +0000

    initializing_project_setup

---
+++ LMS_APP/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Lecture(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lectures')
+    title = models.CharField(max_length=200)
+    vieo_url = models.URLField()
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['student', 'course']
