b7330f67a9c63bedd678bab7d7de741d55ee00dd
Code-with-py <jojarevinay18@gmail.com>
2025-06-19 00:05:46 +0530

    second_commit_some_setup_addon

--- LMS_Project/settings.py
+++ LMS_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_jenkins',
     'LMS_APP',
 ]
 
@@ -122,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JENKINS_TASKS = [
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+]
